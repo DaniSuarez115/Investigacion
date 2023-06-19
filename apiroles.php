69b5bac48ebe5ab0839fdd75bad850c0213bfed9
fabrig97 <fabriciogalo97@gmail.com>
2023-06-18 22:41:37 -0600

    a

--- apiroles.php
+++ apiroles.php
@@ -1,22 +1,29 @@
 <?php
 include_once 'roles.php';
+include_once 'db.php';
 class ApiRoles{
     function getAll()
     {
         $rol=new Roles();
         $rol=array();
-        $rol[""]=array();
+        $rol["items"]=array();
         $respuesta=$rol->obtenerRoles();
         if ($respuesta->rowCount())
         {
             while ($row=$respuesta->fetch(PDO::FETCH_ASSOC))
             {
              $item=array(
-                'id'=>$row['id']
+                'idRol'=>$row['idRol']
              );
              array_push($rol['items'],$items);
             }
-        }else echo json_encode('mesaje'=>'No hay elementos');
+            
+
+        }
+        else 
+        {
+            // echo json_encode('mesaje'->'No hay elementos');
+        }
     }
 }
 
--- roles.php
+++ roles.php
@@ -1,10 +1,17 @@
 <?php
 include_once 'db.php';
-class Roles extends DB
+class Roles extends db
 {
+
     function getRoles()
     {
-        $query=$this->connect()->$query('SELECT * FROM roles desc' );
+        // $query=$this->connect()->$query('SELECT * FROM roles desc');
+
+        $query = $this->connect()->query('SELECT * FROM roles ORDER BY IdRol DESC');
+        return $query;
+        // $query=$this->connect()->$query('SELECT * FROM roles desc');
+
+        return $query;
     }
 }
 
