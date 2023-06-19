02a9ed53abd708ae6d26ce3226e79e3e5404c30a
fabrig97 <fabriciogalo97@gmail.com>
2023-06-18 22:18:23 -0600

    update

--- db.php
+++ db.php
@@ -3,10 +3,11 @@
 $servidor = "localhost";
 $nombreusuario = "root";  
 $password = "123";
+$db = "api";
 $conexion = new mysqli($servidor, $nombreusuario,$password);
 
 if($conexion-> connect_error){
     	die("Conexion fallida: ".$conexion-> connect_error);
 }
-echo "Conexion Exitosa... ";
+echo "Conexion Exitosa... ".$db;
 ?>
